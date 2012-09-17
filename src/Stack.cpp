63cfe0f8cd8357e2e5c9e9c0668880f32b118504
david <david@debian.(none)>
2012-09-17 15:15:55 -0400

    lab 2

--- src/Stack.cpp
+++ src/Stack.cpp
@@ -4,6 +4,7 @@
 Stack::Stack(int initialSize) {
   theStack = new int[initialSize];
   top = 0;
+  fullSize = initialSize;
 }
 
 Stack::~Stack() {
@@ -16,7 +17,17 @@ void Stack::push(int value) {
   // copy all elements to new stack
   // delete old stack
   // point old stack pointer to new stack
-  theStack[top] = value;
+ int* stemp = theStack'
+ if(top >= fullSize) {
+   theStack = new int[fullSize * 2];
+     for(int i = 0; i < top; i++) {
+      theStack[i] = stemp[i];
+	}
+	delete[] stemp;
+	fullSize = fullSize * 2;
+}
+
+ theStack[top] = value;
   top++;
 }
 
--- test/Stack.cpp
+++ test/Stack.cpp
@@ -9,4 +9,15 @@ TEST(StackTest, Push) {
   delete s;
 }
 
-
+TEST(StackTest, PushOver) {
+	Stack* s = new Stack(2);
+	s->push(1);
+	s->push(2);
+	EXPECT_EQ(2, s->size());
+	s->push(3);
+	s->push(4);
+	EXPECT_EQ(4, s->size());
+	s->push(5);
+	EXPECT_EQ(5, s->size());
+	delete s;
+}	
